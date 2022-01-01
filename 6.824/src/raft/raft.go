ebb2b35c396fcc8a82d5120268d83f634183c03e
lsxkugou <lsxkugou@gmail.com>
2021-12-31 22:48:24 -0500

    fix some bug, but still leader > 1 in one term

--- 6.824/src/raft/raft.go
+++ 6.824/src/raft/raft.go
@@ -18,14 +18,18 @@ package raft
 //
 
 import (
-//	"bytes"
+	"fmt"
+	"math/rand"
+	"os"
+
+	//	"bytes"
 	"sync"
 	"sync/atomic"
+	"time"
 
-//	"6.824/labgob"
+	//	"6.824/labgob"
 	"6.824/labrpc"
 )
-
 
 //
 // as each Raft peer becomes aware that successive log entries are
@@ -50,29 +54,65 @@ type ApplyMsg struct {
 	SnapshotIndex int
 }
 
+type raftLog struct {
+	Op   string
+	Term int
+}
+
 //
 // A Go object implementing a single Raft peer.
 //
 type Raft struct {
-	mu        sync.Mutex          // Lock to protect shared access to this peer's state
-	peers     []*labrpc.ClientEnd // RPC end points of all peers
-	persister *Persister          // Object to hold this peer's persisted state
-	me        int                 // this peer's index into peers[]
-	dead      int32               // set by Kill()
+	mu          sync.Mutex          // Lock to protect shared access to this peer's state
+	peers       []*labrpc.ClientEnd // RPC end points of all peers
+	persister   *Persister          // Object to hold this peer's persisted state
+	me          int                 // this peer's index into peers[]
+	dead        int32               // set by Kill()
+	status      int                 // 0 followers, 1 candidate, 2 leader
+	currentTerm int                 // 自己的term
+	voteFor     int                 // 投过的candidateId 每轮term只投给一个人
+	logs        []raftLog           // log
 
-	// Your data here (2A, 2B, 2C).
+	/* 2A leader election */
+	// hearBeat计时器
+	lastHeartBeat    int64 // 上次的Leader heartbeat time
+	heartBeatFre     int64 // leader发送heartbeat的频率20ms
+	heartBeatTimeout int64 // 多久没有收到leader的heartBeat会发起选举 设置为350ms + rand(0,100)ms, 同时也是election timeout
+	//选举计时器
+	electionStartTime int64 //选举开始时间
+	// 选票计数器
+	grantNum int
+
+	/* 2B */
+	/* 2C */
+	/* 2D */
+
 	// Look at the paper's Figure 2 for a description of what
 	// state a Raft server must maintain.
+}
 
+/* 检查 leader 的heartbeat 超时, true代表未超, false代表超时 */
+func (rf *Raft) checkHeartBeat() bool {
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+	if time.Now().UnixMilli()-rf.lastHeartBeat > rf.heartBeatTimeout {
+		return false
+	}
+	return true
 }
 
 // return currentTerm and whether this server
-// believes it is the leader.
+// believes it is the leader. used by tester, don't change it.
 func (rf *Raft) GetState() (int, bool) {
-
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
 	var term int
-	var isleader bool
+	isleader := false
 	// Your code here (2A).
+	term = rf.currentTerm
+	if rf.status == 2 {
+		isleader = true
+	}
 	return term, isleader
 }
 
@@ -91,7 +131,6 @@ func (rf *Raft) persist() {
 	// data := w.Bytes()
 	// rf.persister.SaveRaftState(data)
 }
-
 
 //
 // restore previously persisted state.
@@ -115,7 +154,6 @@ func (rf *Raft) readPersist(data []byte) {
 	// }
 }
 
-
 //
 // A service wants to switch to snapshot.  Only do so if Raft hasn't
 // have more recent info since it communicate the snapshot on applyCh.
@@ -136,13 +174,15 @@ func (rf *Raft) Snapshot(index int, snapshot []byte) {
 
 }
 
-
-//
+/*=================== rpc definition ========================*/
 // example RequestVote RPC arguments structure.
 // field names must start with capital letters!
 //
 type RequestVoteArgs struct {
-	// Your data here (2A, 2B).
+	Term         int // candidate's term
+	CandidateId  int // candidate requesting vote
+	LastLogIndex int // index of candidate's last log entry
+	LastLogTerm  int // term	of candidate's	last log entry
 }
 
 //
@@ -150,14 +190,80 @@ type RequestVoteArgs struct {
 // field names must start with capital letters!
 //
 type RequestVoteReply struct {
-	// Your data here (2A).
+	Term       int  // candidate's term
+	VoteGanted bool // true means candidate received vote
+}
+type AppendEntriesArgs struct {
+	Type         int       // 0 heartBeat, 1 try to synchronize log
+	Term         int       //leader's term
+	LeaderId     int       //so follower can redirect client
+	PrevLogIndex int       //index of log entry immediately preceding new ones
+	PrevLogTerm  int       // term of prevLogIndex entry
+	Entries      []raftLog // entries to store (empty for heartbeat may send more than one for efficiency)
+	LeaderCommit int       //leader's commitIndex
+}
+type AppendEntriesReply struct {
+	Term    int  // currentTerm, for leader to update itself
+	Success bool // true if follower contained entry matching prevLogIndex and prevLogTerm
 }
 
-//
-// example RequestVote RPC handler.
-//
-func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
+/*======================== rpc structure end =================================== */
+
+/*======================== RPC handlers ===========================*/
+
+// RequestVoteHandler handler 用于处理vote请求
+func (rf *Raft) RequestVoteHandler(args *RequestVoteArgs, reply *RequestVoteReply) {
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
 	// Your code here (2A, 2B).
+	reply.Term = rf.currentTerm
+	// 如果请求的term小于自己当前的term,消息过期
+	if args.Term < rf.currentTerm {
+		reply.VoteGanted = false
+		return
+	}
+	//正常处理逻辑
+	// 如果收到了比自己大的term 请求, 则修改自己的term并voteFor置为-1, 如果身份是candidate,回退为followers
+	if args.Term > rf.currentTerm {
+		rf.voteFor = -1
+		rf.currentTerm = args.Term
+		rf.status = 0
+	}
+	myLastLogIndex := -1
+	myLastLogTerm := -1
+	if len(rf.logs) > 0 {
+		myLastLogTerm = rf.logs[len(rf.logs)-1].Term
+		myLastLogIndex = len(rf.logs) - 1
+	}
+	// If votedFor is null or candidateId, and candidate’s log is at least as up-to-date as receiver’s log, grant vote
+	if (rf.voteFor == -1 || rf.voteFor == args.CandidateId) &&
+		args.LastLogIndex >= myLastLogIndex && args.LastLogTerm >= myLastLogTerm {
+		reply.VoteGanted = true
+	} else {
+		reply.VoteGanted = false
+	}
+}
+
+// AppendEntriesHandler 用于处理appendEntries和heartBeat请求
+func (rf *Raft) AppendEntriesHandler(args *AppendEntriesArgs, reply *AppendEntriesReply) {
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+	// 1.如果是heartBeat
+	if args.Type == 0 {
+		rf.lastHeartBeat = time.Now().UnixMilli()
+		// 假设此时状态为candidate且收到了其他人的心跳，且其他人的心跳还大于>=此时自己的,说明发出该心跳的也是一个candidate,并胜选
+		// 如果旧的leader上线, 它的term一定会比candidate竞选的要小,所以可以不考虑
+		if args.Term > rf.currentTerm {
+			rf.currentTerm = args.Term //变为新的term
+			rf.status = 0              // candidate竞选失败 回退至followers 或者 旧leader上线变为followers
+		}
+		return
+	}
+	// 2.如果是日志同步 TODO
+	if args.Type == 1 {
+		return
+	}
+
 }
 
 //
@@ -190,10 +296,19 @@ func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
 // the struct itself.
 //
 func (rf *Raft) sendRequestVote(server int, args *RequestVoteArgs, reply *RequestVoteReply) bool {
-	ok := rf.peers[server].Call("Raft.RequestVote", args, reply)
+	ok := rf.peers[server].Call("Raft.RequestVoteHandler", args, reply)
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+	if reply.VoteGanted == true {
+		rf.grantNum += 1
+	}
+	// 自己回退为followers 另一个candidate的term更大,表示比自己更快
+	if reply.Term > rf.currentTerm {
+		rf.currentTerm = reply.Term
+		rf.status = 0
+	}
 	return ok
 }
-
 
 //
 // the service using Raft (e.g. a k/v server) wants to start
@@ -215,7 +330,6 @@ func (rf *Raft) Start(command interface{}) (int, int, bool) {
 	isLeader := true
 
 	// Your code here (2B).
-
 
 	return index, term, isLeader
 }
@@ -241,6 +355,128 @@ func (rf *Raft) killed() bool {
 	return z == 1
 }
 
+/* ==============三个角色=============== */
+func (rf *Raft) followerBh() {
+	// 1. 查看是否需要进行选举 如果是转化为candidate状态
+	if !rf.checkHeartBeat() {
+		rf.mu.Lock()
+		rf.status = 1
+		rf.mu.Unlock()
+		return
+	}
+	// 2. 处理leader election信息 已由RequestVoteHandler处理
+
+}
+
+func (rf *Raft) candidateBh() {
+	// candidate 有三种情况
+	// 1.选举失败：如果接收到心跳且term大于自己, 变回成followers
+	// 2.如果electionTimeout则重复选举
+	// 3.选举成功：自己成为leader
+
+	for {
+		//1. 退出条件 收到新的leader心跳并发现收到的心跳大于等于自己的currentTerm, 这个逻辑在AppendEntriesHandler里处理,发现这种情况且状态为candidate时, 改变candidate状态
+		//2. 发送leader election rpc
+		//变量设置
+		rf.mu.Lock()
+		if rf.status != 1 {
+			rf.mu.Unlock()
+			break
+		}
+		rf.heartBeatTimeout = GetRdHbTo()
+		rf.electionStartTime = time.Now().UnixMilli()
+		rf.currentTerm += 1
+		rf.grantNum = 1
+		lastLogTerm := -1
+		lastLogIndex := -1
+		if len(rf.logs) > 0 {
+			lastLogTerm = rf.logs[len(rf.logs)-1].Term
+			lastLogIndex = len(rf.logs) - 1
+		}
+		rf.mu.Unlock()
+		// 发送请求 这里对于每个接收者均应开一个线程去处理结果, 不然一个时间等待很长会直接导致选举超时
+		for peer, _ := range rf.peers {
+			if peer == rf.me {
+				continue
+			}
+			rf.mu.Lock()
+			args := RequestVoteArgs{Term: rf.currentTerm, CandidateId: rf.me,
+				LastLogIndex: lastLogIndex, LastLogTerm: lastLogTerm}
+			reply := RequestVoteReply{}
+			rf.mu.Unlock()
+			go rf.sendRequestVote(peer, &args, &reply)
+		}
+		// 超时逻辑
+		rf.mu.Lock()
+		rfElectionStartTime := rf.electionStartTime
+		rfHeartBeatTimeout := rf.heartBeatTimeout
+		rf.mu.Unlock()
+		for time.Now().UnixMilli()-rfElectionStartTime <= rfHeartBeatTimeout {
+			// 成功竞选 leader
+			rf.mu.Lock()
+			if rf.grantNum >= (len(rf.peers)+1)/2 {
+				fmt.Fprintf(os.Stdout, "一共%d个人, 收到%d张票\n", len(rf.peers), rf.grantNum)
+				rf.status = 2
+				rf.mu.Unlock()
+				return
+			}
+			rf.mu.Unlock()
+
+		}
+		// 否则 1.重新开一轮选举同时随机化heartbeat timeout 2.竞选失败 回退为followers
+	}
+}
+
+func (rf *Raft) leaderBh() {
+	for {
+		rf.mu.Lock()
+		if rf.status != 2 {
+			rf.mu.Unlock()
+			break
+		}
+		rf.mu.Unlock()
+		// 1.定时发送heartBeat 每一个peer都需要一个线程进行监听
+		rf.mu.Lock()
+		if time.Now().UnixMilli()-rf.lastHeartBeat > rf.heartBeatFre {
+			for peer, _ := range rf.peers {
+				go rf.sendHeart(peer)
+			}
+		}
+		rf.lastHeartBeat = time.Now().UnixMilli()
+		rf.mu.Unlock()
+
+		// TODO 处理请求以及同步日志部分
+		//for {
+		//
+		//}
+	}
+
+}
+
+/*===============================*/
+
+/*============自定义线程函数================*/
+
+// leader发送心跳
+func (rf *Raft) sendHeart(server int) {
+	// 仅仅当自己还是Leader的时候发送
+	for {
+		rf.mu.Lock()
+		if rf.status != 2 {
+			rf.mu.Unlock()
+			break
+		}
+		rf.mu.Unlock()
+		args := AppendEntriesArgs{Type: 0}
+		reply := AppendEntriesReply{}
+		ok := rf.peers[server].Call("Raft.AppendEntriesHandler", &args, &reply)
+		if !ok {
+			//fmt.Println("========== sendHeart call failed ================")
+		}
+	}
+}
+
+/* =========================== */
 // The ticker go routine starts a new election if this peer hasn't received
 // heartsbeats recently.
 func (rf *Raft) ticker() {
@@ -248,9 +484,26 @@ func (rf *Raft) ticker() {
 
 		// Your code here to check if a leader election should
 		// be started and to randomize sleeping time using
-		// time.Sleep().
+		// time.Sleep()
+		// 根据节点属性处理不一样的事情
+		rf.mu.Lock()
+		raftStatus := rf.status
+		rf.mu.Unlock()
+		switch raftStatus {
+		case 0:
+			rf.followerBh()
+		case 1:
+			rf.candidateBh()
+		case 2:
+			rf.leaderBh()
+		}
 
 	}
+}
+
+// GetRdHbTo 获取随机heartBeatTimeout
+func GetRdHbTo() int64 {
+	return int64(350 + rand.Intn(100))
 }
 
 //
@@ -266,19 +519,18 @@ func (rf *Raft) ticker() {
 //
 func Make(peers []*labrpc.ClientEnd, me int,
 	persister *Persister, applyCh chan ApplyMsg) *Raft {
-	rf := &Raft{}
-	rf.peers = peers
-	rf.persister = persister
-	rf.me = me
-
-	// Your initialization code here (2A, 2B, 2C).
+	// 2A
+	heartBeatTimeout := GetRdHbTo()
+	rf := &Raft{peers: peers, persister: persister, me: me, dead: 0, status: 0,
+		currentTerm: -1, voteFor: -1, lastHeartBeat: time.Now().UnixMilli(),
+		heartBeatFre: 20, heartBeatTimeout: heartBeatTimeout}
 
 	// initialize from state persisted before a crash
 	rf.readPersist(persister.ReadRaftState())
 
 	// start ticker goroutine to start elections
 	go rf.ticker()
-
+	// 建立线程去接收heartBeat,更新rf的变量, 由AppendEntriesHandler RPC去做
 
 	return rf
 }
--- lab2_raft.md
+++ lab2_raft.md
@@ -13,7 +13,12 @@
 #### 五条公理
 
 1. 选举安全特性：对于给定的一个任期号, 最多只会有一个leader被选举出来。每一个replica只有一张选票
-2. Leader只附加原则：
+
+2. Leader只附加原则：leader不会删除或者覆盖自己的日志 只会增加
+
+3. 日志匹配原则：如果两个日志在相同的位置索引号和任期相同，then the logs are identical in all entries up through the given index
+
+   为什么？
 
 
 
@@ -21,35 +26,56 @@
 
 ##### 1.1 流程
 
-![image-20211228085923282](C:\Users\lsxku\Desktop\share_ubuntu\mit6.824\pic\raft-leaderElection.png)
+![image-20211228085923282](.\pic\raft-leaderElection.png)
 
 
 
 1. 每个节点初始状态均为follower, 维护一个计时器, 如果leader的heartbeat在规定时间没到，则发起leader election请求, 且自己变为candidate状态尝试选自己为leader。 如果收到了heartbeat，则重置自己的heartbeat计时器。
+
 2. 在发送选举请求之后,  维护一个选举计时器, 如果在计时器范围内收到majority同意(自己永远投给自己)，则成为leader。成为Leader后同步日志并发送自己的心跳。 如果没有收到majority同意, 则重新开始下一次选举。这里还有一个要注意的, 如果此时有两个以上的candidate试图成为leader，且另一个candidate收到了leader的心跳(在试图成为leader的过程中)，则candidate重新回归followers状态。
+
 3. 如果leader变更之后, 旧的leader重新上线了, 此时集群内就有两个leader了。怎么避免这个问题？ 在leader向followers发送心跳的过程中, 会发送自己的term, 每个followers会记录最大的term, 如果一个followers成为了leader，则该followers的term+1。当旧leader重新上线之后, 旧leader与新leader一起发送心跳(包含自己的term)，旧leader发现自己的term比新的要小，则退位。
 
-4. 在投票中还有一个问题, 如果所有followers的心跳计时器是一样的, 且followers都在同一个时间启动。当leader宕机的时候，所有的followers将会同时发起leader election请求。因为每次选举节点只会把票投给自己，就无法达成共识。要处理这种情况，可以将followers的心跳计时器和选举超时计时器randomlize，比如设置 在 150~300ms均可，这样就不会发生同时超时同时发起选举的问题。每次重置timer的时候，随机化timeout时间
+4. 在投票中还有一个问题, 如果所有followers的心跳计时器是一样的, 且followers都在同一个时间启动。当leader宕机的时候，所有的followers将会同时发起leader election请求。因为每次选举节点只会把票投给自己，就无法达成共识。要处理这种情况，可以将followers的选举超时计时器randomlize，比如设置 在 150~300ms均可，这样就不会发生同时超时同时发起选举的问题。
+
+   
+
+   例如 s1 s2 s3同时选举自己, 此时无法达成共识, 但是因为election timeout是随机的, s1这轮timeout先到了, s1发现上轮没有达成共识，所以重新开始election, 并term + 1, 此时s1的term应该比s2,s3都要大，s2, s3更新自己的termId = s1的，并重置votefor变量为空, 按照论文的伪代码, 此时s1, s2将投票给s1。
+
+   每次重置timer的时候，随机化timeout时间
 
    **restriction：** 
 
    在election过程中, 只有 voter发现自己的log中最大的term小于等于candidate竞选发来的term时，才会投"yes" 原因可以看2.x的第三个例子
-
    
-
+   
+   
+   ##### 1.2实现部分细节
+   
+   1. 每次选完leader之后(或者初始状态), 要对heartBeatTimeOut进行在一定范围内的随机化，以防止多个candidate均试图成为Leader并投票给自己, 达不成共识
+   2. 一个replica在一个term里只能投一张票 如, s1在自己的term 3里只能投一张票
+   3. candidate退出循环的触发是收到了新leader的心跳
+   4. 如果旧leader上线, 收到了新leader的心跳(收到的term > 自己的term), 则应该退回followers
+   5. 如果是candidate, 发现收到了其他的candidate的请求且term大于自己, 则自己回退成followers并投票给它
+   6. 很多情况可以归结于这一条If RPC request or response contains term T > currentTerm: set currentTerm = T, convert to follower (§5.1)
+   
+   
+   
+   
+   
    #### 2. log replication
-
    
+   
+   
+##### 2.x一些可能发生的问题分析以及解决
 
-   ##### 2.x一些可能发生的问题分析以及解决
+1）simplest example
 
-   1）simplest example
-
-   ```
-   s1: 3
-   s2: 3	3	
-   s3:	3	3	
-   ```
+```
+s1: 3
+s2: 3	3	
+s3: 3	3	
+```
 
    假设s3为leader, 为什么此时Log不统一？
 
@@ -58,15 +84,15 @@
    2）leader crash
 
    ```
-   	10	11	12			10	11	12			10	11	12
+	10	11	12			10	11	12			10	11	12
    s1:	3				s1:	3				s1:	3
-   s2:	3	3			s2:	3	3	4		s2:	3	3	4
+s2:	3	3			s2:	3	3	4		s2:	3	3	4
    s3:	3	3			s3:	3	3			s3:	3	3	5
    ```
 
-   **问题复现**
+**问题复现**
 
-   s1 一直因为网络分区, 丢失（11,3）
+s1 一直因为网络分区, 丢失（11,3）
 
    s2当选, term = 4, 收到一个请求, 还没来得及复制, crash
 
@@ -77,54 +103,54 @@
    3）leader crash and solution，leader的slot值高于其他的情况
 
    ```
-   	10	11	12	13
+	10	11	12	13
    s1:	3
-   s2:	3	3	4
+s2:	3	3	4
    s3:	3	3	5	6
    ```
 
-   假设此时s3是leader, 现在到了term6, s3将(13,6)写进log, 并试图让其他子节点进行apply 
+假设此时s3是leader, 现在到了term6, s3将(13,6)写进log, 并试图让其他子节点进行apply 
 
-   leader初始化nextIdx为自己收到最新request的槽位, leader的nextIdx[s2] = 13	nextIdx[s1] = 13
+leader初始化nextIdx为自己收到最新request的槽位, leader的nextIdx[s2] = 13	nextIdx[s1] = 13
 
-   1）leader试图让s2固话(13, 6)并向其发送previous log(12, 5), s2发现自己的最后一个log并非是 (12,5), 所以reject了这个请求, 同理 s1也拒接了这个请求
+1）leader试图让s2固话(13, 6)并向其发送previous log(12, 5), s2发现自己的最后一个log并非是 (12,5), 所以reject了这个请求, 同理 s1也拒接了这个请求
 
-   2)   leader尝试降低nextIdx值去匹配, 如 nextIdx[s2] = 12 此时试图让s2固话(12, 5)(13, 6)并发送previous log(11, 53)，s2发现匹配上了, 接受该请求。并overwrite自己的Log 变为 3 3 5 6
+2)   leader尝试降低nextIdx值去匹配, 如 nextIdx[s2] = 12 此时试图让s2固话(12, 5) (13, 6)并发送previous log(11, 53)，s2发现匹配上了, 接受该请求。并overwrite自己的Log 变为 3 3 5 6
 
-   3）leader将继续降低nextIdx[s1]的值 重复上述步骤 直到s1接受请求
+3）leader将继续降低nextIdx[s1]的值 重复上述步骤 直到s1接受请求
 
-   **问题：**
+**问题：**
 
-   1)s2的4被移除，有关系吗？ 无 因为4并非是majority记录的数据 
+1)s2的4被移除，有关系吗？ 无 因为4并非是majority记录的数据 
 
-   **2) 重点：假设s1为leader会怎么样？它没有足够的log,也不是majority?**
+**2) 重点：假设s1为leader会怎么样？它没有足够的log,也不是majority?**
 
-   解释：因为在election过程中, 只有 voter发现自己的log中最大的term小于等于candidate竞选发来的term时，才会投"yes", 所以 选出来的leadr一定是term比majority要大的, 是可以对majority进行覆盖的
+解释：因为在election过程中, 只有 voter发现自己的log中最大的term小于等于candidate竞选发来的term时，才会投"yes", 所以 选出来的leadr一定是term比majority要大的, 是可以对majority进行覆盖的
 
-   
 
-   
 
-   
 
-   
+
+
 
    
 
-   ##### 1.2数据结构
 
-   **持久化的数据**, 保存进disk里，重启也能记忆
 
-   1. currentTerm：server已知的最大任期, 通过心跳进行更新
+##### 1.2数据结构
+
+**持久化的数据**, 保存进disk里，重启也能记忆
+
+1. currentTerm：server已知的最大任期, 通过心跳进行更新
    2. votedFor:  在leader election中投票给了谁
    3. log[]:  <operation, term, idx>
-
-   **通用性易失数据**
-
-   1.  
-
    
-
+   **通用性易失数据**
+   
+   1.  
+   
+   
+   
    
 
 
@@ -137,7 +163,7 @@
 
 ## 细节部分
 
-1. 由于多个workers要向coordinator请求, 所以coordinator的rpc handler里要加锁 处理concurrency问题
+
 
 
 
@@ -145,11 +171,7 @@
 
 ## Appendix
 
-set up go
+go多线程
 
-https://go.dev/doc/install?download=go1.14.4.linux-amd64.tar.gz 
 
-linux脚本修复
-
-sed -i 's/\r$//' test-mr.sh 
 
